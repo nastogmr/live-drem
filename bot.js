86d36449bc601c643db6be07ff08b12913f2582f
nastogmr <51057724+nastogmr@users.noreply.github.com>
2019-06-18 12:27:34 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : DREAM ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`TEXT`,"http://twitch.tv/Dream")
+client.user.setGame(`ما عندي اسم😩`,"http://twitch.tv/Dream")
 client.user.setStatus("dnd")
 });
 
